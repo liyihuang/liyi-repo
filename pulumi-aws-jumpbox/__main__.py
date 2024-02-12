a61c68f9293d4da1527abb2a5aa7845b1f6c35d3
Liyi Huang <pdshly@gmail.com>
2024-02-12 11:28:37 -0500

    update for setup

--- pulumi-aws-jumpbox/__main__.py
+++ pulumi-aws-jumpbox/__main__.py
@@ -1,23 +1,47 @@
+import sys
+import os
+
+import time
 import pulumi
 import pulumi_aws as aws
 import requests
-import sys
+
 
 key_url = 'https://github.com/liyihuang.keys'
 cloud_init_url = 'https://raw.githubusercontent.com/liyihuang/liyi-cloudinit/main/liyi-init.cfg'
+external_ip = requests.get('https://api.ipify.org', timeout=5).content.decode('utf8')+'/32'
+key_respond = requests.get(key_url,timeout=5)
+cloud_init_respond = requests.get(cloud_init_url,timeout=5)
 
 
-key_respond = requests.get(key_url)
-cloud_init_respond = requests.get(cloud_init_url)
+def setup_local_syncthing(jumpbox_fqdn_id):
+    while True:
+        try:
+            response = requests.get(cloud_init_syncid_url, timeout=5)
+            if response.status_code == 200:
+                print("syncid is now accessible!")
+                sync_id = response.text
+                break
+            else:
+                print("Waiting for syncid to be accessible...")
+                time.sleep(1)
+        except Exception as e:
+            print(f"Error occurred while checking syncid {e}")
+            time.sleep(1)
+    print("start to run local syncthing")
+    # Run a simple shell command
+    os.system(f"syncthing cli config devices add --device-id {sync_id}")
+    os.system(f"syncthing cli config folders default devices add --device-id {sync_id}")
+
 
 if key_respond.status_code != 200 or cloud_init_respond.status_code != 200:
     sys.exit()
 else:
-    cloud_init_data = cloud_init_respond.text
-    public_key = key_respond.text    
+    cloud_init_data = cloud_init_respond.text.replace('liyi-linux','aws_liyi_jumpbox')
+    public_key = key_respond.text
 
-
-key_pair = aws.ec2.KeyPair("liyi-key", public_key=public_key)
+liyi_tags = {'expiry':'2026-01-01','owner':'liyi.huang@isovalent.com'}
+key_pair = aws.ec2.KeyPair("liyi-key", public_key=public_key,tags=liyi_tags)
 
 ubuntu_image = aws.ec2.get_ami(most_recent=True,
     filters=[
@@ -33,13 +57,13 @@ ubuntu_image = aws.ec2.get_ami(most_recent=True,
     owners=["099720109477"])
 
 liyi_jumpbox_sg = aws.ec2.SecurityGroup('liyi_jumpbox_security_group',
-    description='Enable SSH access and allow outgoing traffic',
+    description='Enable incoming traffic and allow outgoing traffic',
     ingress=[
         {
-            'from_port': 22,
-            'to_port': 22,
-            'protocol': 'tcp',
-            'cidr_blocks': ['0.0.0.0/0'],
+            'from_port': 0,
+            'to_port': 0,
+            'protocol': '-1',
+            'cidr_blocks': [external_ip],
         },
     ],
     egress=[
@@ -50,27 +74,36 @@ liyi_jumpbox_sg = aws.ec2.SecurityGroup('liyi_jumpbox_security_group',
             'cidr_blocks': ['0.0.0.0/0'],
 
         }
-    ]
+    ],
+    tags=liyi_tags
 )
 
 liyi_jumpox = aws.ec2.Instance("liyi_jumpbox",
     ami=ubuntu_image.id,
     security_groups=[liyi_jumpbox_sg.name],
     user_data=cloud_init_data,
-    instance_type="t3.medium",
+    instance_type="t3.xlarge",
     root_block_device=aws.ec2.InstanceRootBlockDeviceArgs(
         volume_size=60,  # Set to the desired size of the root volume in GiB
     ),
     key_name=key_pair.key_name,
-    tags={
-        "Name": "liyi-aws-jumpbox"}
+    tags=liyi_tags
 )
 
 
+jumpbox_public_ip = aws.ec2.Instance.get("jumpbox_public_ip",liyi_jumpox.id).public_ip
+
 liyi_fqdn = aws.route53.get_zone(name="liyi.cilium.rocks")
+jumpbox_liyi_fqdn = 'jumpbox.'+aws.route53.get_zone(name="liyi.cilium.rocks").name
+
 jumpbox_fqdn = aws.route53.Record("jumpbox_fqdn",
     zone_id=liyi_fqdn.zone_id,
-    name=f"jumpbox.{liyi_fqdn.name}",
+    name=jumpbox_liyi_fqdn,
     type="A",
     ttl=5,
-    records=[liyi_jumpox.public_ip])
+    records=[jumpbox_public_ip])
+
+cloud_init_http_server_url = 'http://'+jumpbox_liyi_fqdn+':8000/'
+cloud_init_syncid_url = cloud_init_http_server_url+'syncid'
+
+jumpbox_fqdn.id.apply(setup_local_syncthing)
